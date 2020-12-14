ba794e3d200dc4a9de553c66f16fd003182a63df
Fernando Nakatsu <fernando@smartleader.com.br>
2020-12-14 17:20:41 -0300

    criação de migration para account_bank

---
+++ database/migrations/2020_12_14_195218_create_account_bank_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountBankTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_bank', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('account_bank_type_id')->unsigned();
+            $table->bigInteger('balance');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+            $table->foreign('account_bank_type_id')->references('id')->on('account_bank_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_bank');
+    }
+}
